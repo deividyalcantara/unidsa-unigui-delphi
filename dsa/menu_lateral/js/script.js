efd41e9193e535345a9d52deda98256150d6eff1
deividysantos12@hotmail.com <deividysantos12@hotmail.com>
2024-01-23 20:04:05 -0300

    Ajuste

--- dsa/menu_lateral/js/script.js
+++ dsa/menu_lateral/js/script.js
@@ -1,170 +1,279 @@
-var LTamanhoMenuNormal = 250;
-var LTamanhoMenuMinimizado = 34;
-var LManterPosicaoMimizado = 'S';
-var LMinimizado = 'N';
-var LTema = 'C'; // C - CLARO - E - ESCURO
+var LUniDSAMenuLateralTamanhoMenuNormal = 250;
+var LUniDSAMenuLateralTamanhoMenuMinimizado = 34;
+var LUniDSAMenuLateralManterPosicaoMimizado = 'S';
+var LUniDSAMenuLateralMinimizado = 'N';
+var LUniDSAMenuLateralTema = 'T1';
+
+var LUniDSAMenuLateralCorFundo1 = "#FFF";
+var LUniDSAMenuLateralCorBordaFundo1 = "#edeeef";
+var LUniDSAMenuLateralCorIcones1 = "#141414";
+var LUniDSAMenuLateralCorItensFundo1 = "#00000009";
+var LUniDSAMenuLateralCorItens1 = "#141414";
+var LUniDSAMenuLateralCorTexto1 = "#141414";
+var LUniDSAMenuLateralCorTemaTextoSelecionado1 = "#141414";
+var LUniDSAMenuLateralCorIconesHoverSair1 = "#f42e2e";
+var LUniDSAMenuLateralCorFundoNotificacoes1 = '#F42E2E';
+
+var LUniDSAMenuLateralCorFundo2 = "#1C1C1C"; // Fundo escuro
+var LUniDSAMenuLateralCorBordaFundo2 = "#2D2D2D"; // Borda de fundo escuro
+var LUniDSAMenuLateralCorIcones2 = "#FFFFFF"; // Ícones em branco
+var LUniDSAMenuLateralCorItensFundo2 = "#FFFFFF09"; // Fundo dos itens com transparência
+var LUniDSAMenuLateralCorItens2 = "#FFFFFF"; // Texto dos itens em branco
+var LUniDSAMenuLateralCorTexto2 = "#FFFFFF"; // Texto geral em branco
+var LUniDSAMenuLateralCorTemaTextoSelecionado2 = "#FFFFFF"; // Texto selecionado em branco
+var LUniDSAMenuLateralCorIconesHoverSair2 = "#F42E2E"; // Ícones de hover (pode permanecer a mesma cor)
+var LUniDSAMenuLateralCorFundoNotificacoes2 = '#F42E2E';
+
 
 function AlterarTema() {
-  if (LTema == 'E') {
-    $(".uni-ml-config-usuario-tema-2").css("background-color","transparent");           
-    $(".uni-ml-config-usuario-tema-1").css("background-color","#FFF");    
+  if (LUniDSAMenuLateralTema == 'T2') {
+    $(".uni-ml-config-usuario-tema-1").css("background-color", "transparent");
+    $(".uni-ml-config-usuario-tema-2").css("background-color", "#FFF");
 
-    $(".uni-ml-config-usuario-tema-1 span").css("color","var(--var-uni-ml-cor-tema-texto)");            
-    $(".uni-ml-config-usuario-tema-2 span").css("color","var(--var-uni-ml-cor-tema-texto-selecionado)");      
+    $(".uni-ml-config-usuario-tema-2 span").css("color", "var(--var-uni-ml-cor-tema-texto)");
+    $(".uni-ml-config-usuario-tema-1 span").css("color", "var(--var-uni-ml-cor-tema-texto-selecionado)");
 
-    $(".uni-ml-config-usuario-tema-1 i").css("color","var(--var-uni-ml-cor-tema-texto)");                 
-    $(".uni-ml-config-usuario-tema-2 i").css("color","var(--var-uni-ml-cor-tema-texto-selecionado)");      
-
-    LTema = 'C';
+    $(".uni-ml-config-usuario-tema-2 i").css("color", "var(--var-uni-ml-cor-tema-texto)");
+    $(".uni-ml-config-usuario-tema-1 i").css("color", "var(--var-uni-ml-cor-tema-texto-selecionado)");
   } else {
+    $(".uni-ml-config-usuario-tema-2").css("background-color", "transparent");
+    $(".uni-ml-config-usuario-tema-1").css("background-color", "#FFF");
 
-    $(".uni-ml-config-usuario-tema-1").css("background-color","transparent");
-    $(".uni-ml-config-usuario-tema-2").css("background-color","#FFF"); 
+    $(".uni-ml-config-usuario-tema-1 span").css("color", "var(--var-uni-ml-cor-tema-texto)");
+    $(".uni-ml-config-usuario-tema-2 span").css("color", "var(--var-uni-ml-cor-tema-texto-selecionado)");
 
-    $(".uni-ml-config-usuario-tema-2 span").css("color","var(--var-uni-ml-cor-tema-texto)");      
-    $(".uni-ml-config-usuario-tema-1 span").css("color","var(--var-uni-ml-cor-tema-texto-selecionado)");           
-
-    $(".uni-ml-config-usuario-tema-2 i").css("color","var(--var-uni-ml-cor-tema-texto)");      
-    $(".uni-ml-config-usuario-tema-1 i").css("color","var(--var-uni-ml-cor-tema-texto-selecionado)");   
-    
-    LTema = 'E';      
+    $(".uni-ml-config-usuario-tema-1 i").css("color", "var(--var-uni-ml-cor-tema-texto)");
+    $(".uni-ml-config-usuario-tema-2 i").css("color", "var(--var-uni-ml-cor-tema-texto-selecionado)");
   };
 };
 
-function DefinirCorTema(ACorDeFundo, ACorDaBordaFundo, ACorIcones, ACorItensFundo, ACorItens, ACorTexto, ACorTemaTextoSelecionado, ACorIconeHoverSair) {
-  var root = document.querySelector(":root");
+function ConfigurarCorTema(
+  LUniDSAMenuLateralTema,
+  LUniDSAMenuLateralCorFundo,
+  LUniDSAMenuLateralCorBordaFundo,
+  LUniDSAMenuLateralCorIcones,
+  LUniDSAMenuLateralCorItensFundo,
+  LUniDSAMenuLateralCorItens,
+  LUniDSAMenuLateralCorTexto,
+  LUniDSAMenuLateralCorTemaTextoSelecionado,
+  LUniDSAMenuLateralCorIconesHoverSair,
+  LUniDSAMenuLateralCorFundoNotificacoes
+) {
+  if (LUniDSAMenuLateralTema == 'T1') {
+    LUniDSAMenuLateralCorFundo1 = LUniDSAMenuLateralCorFundo;
+    LUniDSAMenuLateralCorBordaFundo1 = LUniDSAMenuLateralCorBordaFundo;
+    LUniDSAMenuLateralCorIcones1 = LUniDSAMenuLateralCorIcones;
+    LUniDSAMenuLateralCorItensFundo1 = LUniDSAMenuLateralCorItensFundo;
+    LUniDSAMenuLateralCorItens1 = LUniDSAMenuLateralCorItens;
+    LUniDSAMenuLateralCorTexto1 = LUniDSAMenuLateralCorTexto;
+    LUniDSAMenuLateralCorTemaTextoSelecionado1 = LUniDSAMenuLateralCorTemaTextoSelecionado;
+    LUniDSAMenuLateralCorIconesHoverSair1 = LUniDSAMenuLateralCorIconesHoverSair;
+    LUniDSAMenuLateralCorFundoNotificacoes1 = LUniDSAMenuLateralCorFundoNotificacoes;
+  } else {
+    LUniDSAMenuLateralCorFundo2 = LUniDSAMenuLateralCorFundo;
+    LUniDSAMenuLateralCorBordaFundo2 = LUniDSAMenuLateralCorBordaFundo;
+    LUniDSAMenuLateralCorIcones2 = LUniDSAMenuLateralCorIcones;
+    LUniDSAMenuLateralCorItensFundo2 = LUniDSAMenuLateralCorItensFundo;
+    LUniDSAMenuLateralCorItens2 = LUniDSAMenuLateralCorItens;
+    LUniDSAMenuLateralCorTexto2 = LUniDSAMenuLateralCorTexto;
+    LUniDSAMenuLateralCorTemaTextoSelecionado2 = LUniDSAMenuLateralCorTemaTextoSelecionado;
+    LUniDSAMenuLateralCorIconesHoverSair2 = LUniDSAMenuLateralCorIconesHoverSair;
+    LUniDSAMenuLateralCorFundoNotificacoes2 = LUniDSAMenuLateralCorFundoNotificacoes;
+  }
 
-  root.style.setProperty("--var-uni-ml-cor-bg", ACorDeFundo);
-  root.style.setProperty("--var-uni-ml-cor-icones", ACorIcones);    
-  root.style.setProperty("--var-uni-ml-cor-itens-fundo", ACorItensFundo);    
-  root.style.setProperty("--var-uni-ml-cor-itens", ACorItens);   
-  root.style.setProperty("--var-uni-ml-cor-texto", ACorTexto);  
-  root.style.setProperty("--var-uni-ml-cor-tema-texto-selecionado",ACorTemaTextoSelecionado); 
-  root.style.setProperty("--var-uni-ml-cor-icone-sair", ACorIconeHoverSair);     
-  root.style.setProperty("--var-uni-ml-cor-borda", ACorDaBordaFundo);   
-     
   AlterarTema();
-};    
-
-function TemaPadrao() {
-  DefinirCorTema("#0a4ef5", "#000", "#FFF", "#00000021", "#FFF", "#FFF", "#FFF", "#f42e2e");
 };
 
-function MenuLateralMinimizado(){
-  LMinimizado = 'S';
+function DefinirCorTema(
+  LUniDSAMenuLateralCorFundo,
+  LUniDSAMenuLateralCorBordaFundo,
+  LUniDSAMenuLateralCorIcones,
+  LUniDSAMenuLateralCorItensFundo,
+  LUniDSAMenuLateralCorItens,
+  LUniDSAMenuLateralCorTexto,
+  LUniDSAMenuLateralCorTemaTextoSelecionado,
+  LUniDSAMenuLateralCorIconesHoverSair,
+  LUniDSAMenuLateralCorFundoNotificacoes
+) {
+  var root = document.querySelector(":root");
 
-  document.documentElement.style.setProperty('--var-ml-tamanho', LTamanhoMenuMinimizado + 'px');   
+  root.style.setProperty("--var-uni-ml-cor-bg", LUniDSAMenuLateralCorFundo);
+  root.style.setProperty("--var-uni-ml-cor-icones", LUniDSAMenuLateralCorIcones);
+  root.style.setProperty("--var-uni-ml-cor-itens-fundo", LUniDSAMenuLateralCorItensFundo);
+  root.style.setProperty("--var-uni-ml-cor-itens", LUniDSAMenuLateralCorItens);
+  root.style.setProperty("--var-uni-ml-cor-texto", LUniDSAMenuLateralCorTexto);
+  root.style.setProperty("--var-uni-ml-cor-tema-texto-selecionado", LUniDSAMenuLateralCorTemaTextoSelecionado);
+  root.style.setProperty("--var-uni-ml-cor-icone-sair", LUniDSAMenuLateralCorIconesHoverSair);
+  root.style.setProperty("--var-uni-ml-cor-borda", LUniDSAMenuLateralCorBordaFundo);
+  root.style.setProperty("--var-uni-ml-cor-notificacao", LUniDSAMenuLateralCorFundoNotificacoes);
+
+  AlterarTema();
+};
+
+function Tema1() {
+  LUniDSAMenuLateralTema = 'T1';
+
+  DefinirCorTema(
+    LUniDSAMenuLateralCorFundo1,
+    LUniDSAMenuLateralCorBordaFundo1,
+    LUniDSAMenuLateralCorIcones1,
+    LUniDSAMenuLateralCorItensFundo1,
+    LUniDSAMenuLateralCorItens1,
+    LUniDSAMenuLateralCorTexto1,
+    LUniDSAMenuLateralCorTemaTextoSelecionado1,
+    LUniDSAMenuLateralCorIconesHoverSair1,
+    LUniDSAMenuLateralCorFundoNotificacoes1
+  );
+};
+
+function Tema2() {
+  LUniDSAMenuLateralTema = 'T2';
+
+  DefinirCorTema(
+    LUniDSAMenuLateralCorFundo2,
+    LUniDSAMenuLateralCorBordaFundo2,
+    LUniDSAMenuLateralCorIcones2,
+    LUniDSAMenuLateralCorItensFundo2,
+    LUniDSAMenuLateralCorItens2,
+    LUniDSAMenuLateralCorTexto2,
+    LUniDSAMenuLateralCorTemaTextoSelecionado2,
+    LUniDSAMenuLateralCorIconesHoverSair2,
+    LUniDSAMenuLateralCorFundoNotificacoes2
+  );
+};
+
+function MenuLateralMinimizado() {
+  LUniDSAMenuLateralMinimizado = 'S';
+
+  document.documentElement.style.setProperty('--var-ml-tamanho', LUniDSAMenuLateralTamanhoMenuMinimizado + 'px');
   $("#uni-ml-logo-nome-empresa").hide();
   $("#uni-ml-pesquisar").hide();
   $(".uni-ml-pesquisa").css("padding-right", "0");
   $(".uni-ml-scroll-menu ul li span").hide();
-  $(".uni-ml-scroll-menu ul li a").css("margin-right","0");
+  $(".uni-ml-scroll-menu ul li a").css("margin-right", "0");
+  $(".uni-ml-scroll-menu").css("overflow-y", "hidden");
+  $(".uni-ml-scroll-menu ul").css("overflow-y", "hidden");
 
-  if (LManterPosicaoMimizado == 'S') {
-    $(".uni-ml-scroll-menu .uni-ml-titulo-menu").css("visibility","hidden");
+  if (LUniDSAMenuLateralManterPosicaoMimizado == 'S') {
+    $(".uni-ml-scroll-menu .uni-ml-titulo-menu").css("visibility", "hidden");
     $(".uni-ml-scroll-menu .uni-ml-titulo-menu").show();
   } else {
-    $(".uni-ml-scroll-menu .uni-ml-titulo-menu").css("visibility","visible");
+    $(".uni-ml-scroll-menu .uni-ml-titulo-menu").css("visibility", "visible");
     $(".uni-ml-scroll-menu .uni-ml-titulo-menu").hide();
   };
 
-  $("#uni-ml-item-menu-notif").css("position","absolute");
-  $("#uni-ml-item-menu-notif").css("margin-top","20px");
-  $("#uni-ml-item-menu-notif").css("margin-left","18px");    
-  $("#uni-ml-item-menu-notif").css("border-radius","50px");      
-  $("#uni-ml-item-menu-notif").css("width","18px");   
+  $("#uni-ml-item-menu-notif").css("position", "absolute");
+  $("#uni-ml-item-menu-notif").css("margin-top", "20px");
+  $("#uni-ml-item-menu-notif").css("margin-left", "18px");
+  $("#uni-ml-item-menu-notif").css("border-radius", "50px");
+  $("#uni-ml-item-menu-notif").css("width", "18px");
 
-  $(".fa-chevron-right").hide(); 
-  
-  $(".uni-ml-config-usuario-tema").css("flex-direction","column");
-  $(".uni-ml-config-usuario-tema").css("min-height","calc(var(--var-ml-altura-itens) + var(--var-ml-altura-itens))");    
+  $(".fa-chevron-right").hide();
+
+  $(".uni-ml-config-usuario-tema").css("flex-direction", "column");
+  $(".uni-ml-config-usuario-tema").css("min-height", "calc(var(--var-ml-altura-itens) + var(--var-ml-altura-itens))");
 
   $(".uni-ml .uni-ml-config-usuario .uni-ml-config-usuario-tema .uni-ml-config-usuario-tema-1 span").hide();
-  $(".uni-ml .uni-ml-config-usuario .uni-ml-config-usuario-tema .uni-ml-config-usuario-tema-2 span").hide();  
-  $(".uni-ml .uni-ml-config-usuario .uni-ml-config-usuario-tema .uni-ml-config-usuario-tema-1 i").css("height","min-height: var(--var-ml-altura-itens)");
-  $(".uni-ml .uni-ml-config-usuario .uni-ml-config-usuario-tema .uni-ml-config-usuario-tema-2 i").css("height","min-height: var(--var-ml-altura-itens)");  
-  
+  $(".uni-ml .uni-ml-config-usuario .uni-ml-config-usuario-tema .uni-ml-config-usuario-tema-2 span").hide();
+  $(".uni-ml .uni-ml-config-usuario .uni-ml-config-usuario-tema .uni-ml-config-usuario-tema-1 i").css("height", "min-height: var(--var-ml-altura-itens)");
+  $(".uni-ml .uni-ml-config-usuario .uni-ml-config-usuario-tema .uni-ml-config-usuario-tema-2 i").css("height", "min-height: var(--var-ml-altura-itens)");
+
   $(".uni-ml-perfil-desc").hide();
   $(".uni-ml-perfil i").hide();
 
-  $(".uni-ml .uni-ml-config-usuario .uni-ml-perfil img").css("height","24px");
-}; 
+  $(".uni-ml .uni-ml-config-usuario .uni-ml-perfil img").css("height", "24px");
+};
 
-function MenuLateralMaximizado(){
-  LMinimizado = 'N';
+function MenuLateralMaximizado() {
+  LUniDSAMenuLateralMinimizado = 'N';
 
-  document.documentElement.style.setProperty('--var-ml-tamanho', LTamanhoMenuNormal + 'px');  
+  document.documentElement.style.setProperty('--var-ml-tamanho', LUniDSAMenuLateralTamanhoMenuNormal + 'px');
   $("#uni-ml-logo-nome-empresa").show();
   $("#uni-ml-pesquisar").show();
   $(".uni-ml-pesquisa").css("padding-right", "15px");
-  $(".uni-ml-scroll-menu ul li span").show();    
+  $(".uni-ml-scroll-menu ul li span").show();
   $(".uni-ml-scroll-menu .uni-ml-titulo-menu").show();
-  $(".uni-ml-scroll-menu .uni-ml-titulo-menu").css("visibility","visible");
+  $(".uni-ml-scroll-menu .uni-ml-titulo-menu").css("visibility", "visible");
   $(".uni-ml-scroll-menu .uni-ml-titulo-menu").show();
-  $(".uni-ml-scroll-menu ul li a").css("margin-right","8px");    
+  $(".uni-ml-scroll-menu ul li a").css("margin-right", "8px");
+  $(".uni-ml-scroll-menu").css("overflow-y", "auto");
+  $(".uni-ml-scroll-menu ul").css("overflow-y", "auto");
 
-  $("#uni-ml-item-menu-notif").css("position","");
-  $("#uni-ml-item-menu-notif").css("margin-top","");
-  $("#uni-ml-item-menu-notif").css("margin-left","");    
-  $("#uni-ml-item-menu-notif").css("border-radius","8px");      
-  $("#uni-ml-item-menu-notif").css("width","30px");   
-  
+  $("#uni-ml-item-menu-notif").css("position", "");
+  $("#uni-ml-item-menu-notif").css("margin-top", "");
+  $("#uni-ml-item-menu-notif").css("margin-left", "");
+  $("#uni-ml-item-menu-notif").css("border-radius", "8px");
+  $("#uni-ml-item-menu-notif").css("width", "30px");
+
   $(".fa-chevron-right").show();
 
-  $(".uni-ml-config-usuario-tema").css("flex-direction","row");    
-  $(".uni-ml-config-usuario-tema").css("min-height","var(--var-ml-altura-itens)");      
+  $(".uni-ml-config-usuario-tema").css("flex-direction", "row");
+  $(".uni-ml-config-usuario-tema").css("min-height", "var(--var-ml-altura-itens)");
 
   $(".uni-ml .uni-ml-config-usuario .uni-ml-config-usuario-tema .uni-ml-config-usuario-tema-1 span").show();
-  $(".uni-ml .uni-ml-config-usuario .uni-ml-config-usuario-tema .uni-ml-config-usuario-tema-2 span").show();    
-  $(".uni-ml .uni-ml-config-usuario .uni-ml-config-usuario-tema .uni-ml-config-usuario-tema-1 i").css("height","");
-  $(".uni-ml .uni-ml-config-usuario .uni-ml-config-usuario-tema .uni-ml-config-usuario-tema-2 i").css("height",""); 
+  $(".uni-ml .uni-ml-config-usuario .uni-ml-config-usuario-tema .uni-ml-config-usuario-tema-2 span").show();
+  $(".uni-ml .uni-ml-config-usuario .uni-ml-config-usuario-tema .uni-ml-config-usuario-tema-1 i").css("height", "");
+  $(".uni-ml .uni-ml-config-usuario .uni-ml-config-usuario-tema .uni-ml-config-usuario-tema-2 i").css("height", "");
+  $(".uni-ml .uni-ml-config-usuario .uni-ml-config-usuario-tema").css("height", "calc(var(--var-ml-altura-itens) + 6px)");
 
-  $(".uni-ml-perfil-desc").show(); 
-  $(".uni-ml-perfil i").show();   
+  $(".uni-ml-perfil-desc").show();
+  $(".uni-ml-perfil i").show();
 
-  $(".uni-ml .uni-ml-config-usuario .uni-ml-perfil img").css("height","var(--var-ml-altura-itens)");
-};   
+  $(".uni-ml .uni-ml-config-usuario .uni-ml-perfil img").css("height", "var(--var-ml-altura-itens)");
+};
 
-$(document).ready(function() {
-  function GetValorVariavel(AVariavel) {
-    const LRootStyles = getComputedStyle(document.documentElement);
-    const LValorEmPixel = LRootStyles.getPropertyValue(AVariavel);
-    return parseInt(LValorEmPixel);
+
+function UniDSAMenuLateralOnClickLogo(AJSName) {
+  ajaxRequest(AJSName, 'UniDSAMenuLateralOnClickLogo', []);
+};
+
+function UniDSAMenuLateralOnClickMenu(AJSName, ANomeMenu) {
+  ajaxRequest(AJSName, 'UniDSAMenuLateralOnClickMenu', ["menu=" + ANomeMenu]);
+};
+
+function UniDSAMenuLateralOnClickNotificationMenu(AJSName, ANomeMenu, AEvent) {
+  ajaxRequest(AJSName, 'UniDSAMenuLateralOnClickNotificationMenu', ["menu=" + ANomeMenu]);
+  AEvent.stopPropagation();
+};
+
+function UniDSAMenuLateralOnAfterSelectTheme(AJSName, AEvent) {
+  var LOpcao = AEvent.target.getAttribute("option");
+
+  AlterarTema();
+
+  if (LOpcao == 'T2') {
+    Tema2();
+  } else {
+    Tema1();
   };
-  
-  $(window).on("resize", function() {
-    var larguraUniML = $(".uni-ml").width();
 
-    if (larguraUniML <= 34) {
-      MenuLateralMinimizado();
-    } else {
-      MenuLateralMaximizado();
-    }
-  });
-    
-  /* Minimizar ou maximizar menu */
-  $(".uni-ml-logo").click(function() {
-    if (GetValorVariavel("--var-ml-tamanho") == LTamanhoMenuMinimizado) {
-      MenuLateralMaximizado()
-    } else {
-      MenuLateralMinimizado();   
-    }
-  });
-   
-  $(".uni-ml-pesquisa").click(function(){
-    if (LMinimizado == 'S') {
-      MenuLateralMaximizado();
-      $("#uni-ml-pesquisar").focus();
-    };
-  });
+  ajaxRequest(AJSName, 'UniDSAMenuLateralOnAfterSelectTheme', ["tema=" + LOpcao]);
+};
 
-  $(".uni-ml-config-usuario-tema-1").click(function () {  
-    AlterarTema();
-    TemaBlack();    
-  });
+function UniDSAMenuLateralOnClickProfile(AJSName) {
+  ajaxRequest(AJSName, 'UniDSAMenuLateralOnClickProfile', []);
+};
 
-  $(".uni-ml-config-usuario-tema-2").click(function () {  
-    AlterarTema();
-    TemaBlack();
-  });  
-});
+function UniDSAMenuLateralOnClickLogoff(AJSName) {
+  ajaxRequest(AJSName, 'UniDSAMenuLateralOnClickLogoff', []);
+};
+
+function UniDSAMenuLateralOnSearchEnter(AJSName, AValor) {
+  ajaxRequest(AJSName, 'UniDSAMenuLateralOnSearchEnter', ["texto=" + AValor]);
+};
+
+function UniDSAMenuLateralOnClickIconSearch(AJSName) {
+  ajaxRequest(AJSName, 'UniDSAMenuLateralOnClickIconSearch', []);
+};
+
+function UniDSAMenuLateralOnKeyUpSearch(AJSName, AEvent) {
+  ajaxRequest(AJSName, 'UniDSAMenuLateralGetSearchValue', ["texto=" + AEvent.target.value]);
+};
+
+function UniDSAMenuLateralOnKeyDownSearch(AJSName, AEvent) {
+  if (AEvent.keyCode === 13) {
+    const LValorInput = AEvent.target.value;
+
+    UniDSAMenuLateralOnSearchEnter(AJSName, LValorInput)
+  }
+};
